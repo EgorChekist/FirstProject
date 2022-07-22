5f4beee3caba75bddb7c834d7330638dbf780e19
user <danil369.ru@mail.ru>
2022-07-22 11:55:45 +0600

    Задания

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="homepage.html"))
+    path("", TemplateView.as_view(template_name="homepage.html")),
+    path("secondpage/", TemplateView.as_view(template_name="secondpage.html")),
 ]
