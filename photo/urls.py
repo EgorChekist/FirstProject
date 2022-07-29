e8f2ac2073863b6fe454a2e9b741073ee6204d0e
user <danil369.ru@mail.ru>
2022-07-29 11:48:15 +0600

    Закончили план 1

--- photo/urls.py
+++ photo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
+from photo.views import photos
 
 urlpatterns = [
-
+    path("", photos)
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def photos(request):
+    data = {}
+    data["secret"] = random.randint(1, 100)
+    data["locations"] = ["поле", "лес", "речка", "дамбу", "деревня", "огород"]
+    return render(request, "photos.html", data)
