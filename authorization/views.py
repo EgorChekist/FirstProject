50193652d35c497b85f58d77d7919395c113e537
user <danil369.ru@mail.ru>
2022-09-10 11:53:35 +0600

    Повторение

--- authorization/views.py
+++ authorization/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 def login(request):
     return render(request, "login.html")
 
 def login_handle(request):
-    print(request.POST)
+    data = {}
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    user = User.objects.filter(username=username)
+    if user.exists():
+        print("Пользователь найден")
+    else:
+        data["error"] = "Пользователь не найден"
+        return render(request, "login.html", data)
+
     return redirect("/auth/login/")
 
 def registration(request):
